41098b29bdc9a1af3d7b6083c06c1e266012ffd2
agral <adam@gralin.ski>
2019-10-21 21:44:16 +0200

    Update answer to HR's BiggerIsGreater

---
+++ HackerRank/Practice/Algorithms/Implementation/BiggerIsGreater/ans.cpp
@@ -0,0 +1,16 @@
+/**
+ * Solution to the "Bigger is Greater" challenge form HackerRank:
+ * https://www.hackerrank.com/challenges/bigger-is-greater/problem
+ * Created on: 21.10.2019
+ * Last modified: 21.10.2019
+ * Author: Adam Gralinski (adam@gralin.ski)
+ * License: MIT
+ */
+
+#include <iostream>
+
+int main(int, char**)
+{
+  testcases
+  return 0;
+}
