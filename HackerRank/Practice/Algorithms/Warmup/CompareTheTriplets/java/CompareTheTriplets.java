916bf82dcf48a58f6f66d0d82cbe432698f0eaca
agral <adam@gralin.ski>
2020-03-20 23:20:56 +0100

    Add java solution to Algo/Warmup/CompareTheTriplets

--- HackerRank/Practice/Algorithms/Warmup/CompareTheTriplets/java/CompareTheTriplets.java
+++ HackerRank/Practice/Algorithms/Warmup/CompareTheTriplets/java/CompareTheTriplets.java
@@ -2,7 +2,7 @@
  * Solution to the "Compare the Triplets" challenge from HackerRank:
  * https://www.hackerrank.com/challenges/compare-the-triplets/problem
  * Created on: 20.03.2020
- * Last modified: $20.03.2020
+ * Last modified: 20.03.2020
  * Author: Adam Graliński (adam@gralin.ski)
  * License: MIT
  */
@@ -14,7 +14,24 @@ public class CompareTheTriplets
 {
   private static final Scanner scanner = new Scanner(System.in);
 
+  public static final int SIZE = 3;
+
   public static void main(String[] args) {
-    
+    int[] alice = new int[SIZE];
+    int scoreAlice = 0, scoreBob = 0;
+    for (int n = 0; n < SIZE; ++n) {
+      alice[n] = scanner.nextInt();
+    }
+    for (int n = 0; n < SIZE; ++n) {
+      int bob = scanner.nextInt();
+      if (alice[n] > bob) {
+        scoreAlice += 1;
+      }
+      else if (alice[n] < bob) {
+        scoreBob += 1;
+      }
+    }
+
+    System.out.println(scoreAlice + " " + scoreBob);
   }
 }
