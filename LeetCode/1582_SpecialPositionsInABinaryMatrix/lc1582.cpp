9f69b4af3ea5fd74629a2c55f3eb14d4fb2402ae
agral <adam@gralin.ski>
2023-12-13 22:06:24 +0100

    [LeetCode] Solve 1582/SpecialPositionsInABinaryMatrix
    An uglier solution than the one conceived initially,
    but this one works. Requires double pass of the entire matrix.
    Time: O(m * n)
    Memory: O(m+n)

--- LeetCode/1582_SpecialPositionsInABinaryMatrix/lc1582.cpp
+++ LeetCode/1582_SpecialPositionsInABinaryMatrix/lc1582.cpp
@@ -6,32 +6,27 @@ public:
   int numSpecial(std::vector<std::vector<int>>& mat) {
     const auto HEIGHT = mat.size();
     const auto WIDTH = mat[0].size();
-    int sole_ones_in_rows = 0;
-    int sole_ones_in_columns = 0;
+    std::vector<int> rowSum(HEIGHT, 0); // holds sum of each row, column-by-column (row stays the same)
+    std::vector<int> colSum(WIDTH, 0);
     for (int h = 0; h < HEIGHT; h++) {
-      int count = 0;
       for (int w = 0; w < WIDTH; w++) {
         if (mat[h][w] == 1) {
-          ++count;
+          ++rowSum[h];
+          ++colSum[w];
         }
-      }
-      if (count == 1) {
-        ++sole_ones_in_rows;
-      }
-    }
-    for (int w = 0; w < WIDTH; w++) {
-      int count = 0;
-      for (int h = 0; h < HEIGHT; h++) {
-        if (mat[h][w] == 1) {
-          ++count;
-        }
-      }
-      if (count == 1) {
-        ++sole_ones_in_columns;
       }
     }
 
-    return std::min(sole_ones_in_columns, sole_ones_in_rows);
+    int ans = 0;
+    for (int h = 0; h < HEIGHT; h++) {
+      for (int w = 0; w < WIDTH; w++) {
+        if ((mat[h][w] == 1) && (rowSum[h] == 1) && (colSum[w] == 1)) {
+          ++ans;
+        }
+      }
+    }
+
+    return ans;
   }
 };
 
