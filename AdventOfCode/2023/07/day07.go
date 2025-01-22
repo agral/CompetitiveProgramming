e5f8d7be867d5045741a1b262aebdb0a329224ef
agral <adam@gralin.ski>
2025-01-22 17:04:01 +0100

    Solve AoC2023, Day07 / Gold; in Golang

--- AdventOfCode/2023/07/day07.go
+++ AdventOfCode/2023/07/day07.go
@@ -25,6 +25,21 @@ var CARD_SENIORITY = map[byte]int{
 	'K': 13,
 	'A': 14,
 }
+var CARD_SENIORITY_GOLD = map[byte]int{
+	'J': 1,
+	'2': 2,
+	'3': 3,
+	'4': 4,
+	'5': 5,
+	'6': 6,
+	'7': 7,
+	'8': 8,
+	'9': 9,
+	'T': 10,
+	'Q': 11,
+	'K': 12,
+	'A': 13,
+}
 
 type HandType int
 
@@ -40,13 +55,14 @@ const (
 )
 
 type Deal struct {
-	Hand string
-	Bid  int
-	Type HandType
+	Hand     string
+	Bid      int
+	Type     HandType
+	GoldType HandType
 }
 
 func MakeDeal(hand string, bid int) Deal {
-	// Determine the instance's HandType:
+	// Determine the instance's HandType (no jokers introduced):
 	handType := HIGH_CARD
 	counts := make(map[byte]int)
 	for i := 0; i < len(hand); i++ {
@@ -72,10 +88,75 @@ func MakeDeal(hand string, bid int) Deal {
 		handType = ONE_PAIR
 	}
 
+	// Determine the instance's "gold" HandType (new joker rule):
+	goldType := HIGH_CARD
+	goldCounts := make(map[byte]int)
+	for i := 0; i < len(hand); i++ {
+		goldCounts[hand[i]]++
+	}
+	goldCombos := make([]int, 6)
+	for k, v := range goldCounts {
+		if k != 'J' {
+			goldCombos[v]++
+		}
+	}
+
+	if goldCombos[5] == 1 {
+		goldType = FIVE_OF_A_KIND
+	} else if goldCombos[4] == 1 {
+		goldType = FOUR_OF_A_KIND
+	} else if goldCombos[3] == 1 {
+		if goldCombos[2] == 1 {
+			goldType = FULL_HOUSE
+		} else {
+			goldType = THREE_OF_A_KIND
+		}
+	} else if goldCombos[2] == 2 {
+		goldType = TWO_PAIR
+	} else if goldCombos[2] == 1 {
+		goldType = ONE_PAIR
+	}
+
+	for i := 0; i < goldCounts['J']; i++ {
+		// Joker ascension rules:
+		switch goldType {
+		case HIGH_CARD:
+			goldType = ONE_PAIR // from e.g. 789TJ to e.g. 789TT, best result is one pair.
+		case ONE_PAIR:
+			// from e.g. TTJQK can go either two pairs (TTQQK) or three-of-a-kind (TTTQK),
+			goldType = THREE_OF_A_KIND // and three-of-a-kind is best.
+		case TWO_PAIR:
+			goldType = FULL_HOUSE // from e.g. TTJQQ best result is TTQQQ
+		case THREE_OF_A_KIND:
+			goldType = FOUR_OF_A_KIND // from e.g. TTTJQ best result is TTTTQ
+		case FULL_HOUSE: // a full house e.g. TTTJJ or TTJJJ will not be reported as a full house,
+			// because J cards are not counted for goldCounts. So, seeing any ascension from a FULL_HOUSE
+			// would be a logic error.
+			fmt.Printf("Hand: %s\n", hand)
+			panic("Ascension from full house should never happen!")
+		case FOUR_OF_A_KIND: // from e.g. TTJTT to TTTTT
+			goldType = FIVE_OF_A_KIND
+		case FIVE_OF_A_KIND: // the only ascension that can happen from FIVE_OF_A_KIND
+			// Due to the way converting hands to hand types is realized,
+			// a hand of exclusively "JJJJJ" is initially sorted as HIGH_CARD, and then
+			// ascended five times: to ONE_PAIR (1), THREE_OF_A_KIND (2), FOUR_OF_A_KIND (3)
+			// and then twice to FIVE_OF_A_KIND (4, 5). This is valid only if starting from five jacks;
+			// any other five-of-a-kind can not ascend.
+			if hand == "JJJJJ" {
+				goldType = FIVE_OF_A_KIND // won't get better
+			}
+			if hand != "JJJJJ" {
+				fmt.Printf("Hand: %s\n", hand)
+				panic("Ascension from five-of-a-kind that is not \"JJJJJ\" should never happen")
+			}
+		}
+	}
+
 	return Deal{
-		Hand: hand,
-		Bid:  bid,
-		Type: handType,
+		Hand:     hand,
+		Bid:      bid,
+		Type:     handType,
+		GoldType: goldType,
 	}
 }
 
@@ -117,5 +198,21 @@ func main() {
 		silver += (i + 1) * deal.Bid
 	}
 	fmt.Printf("Silver: %d\n", silver)
+
+	// Gold part is same as silver, but sort by joker rule (GoldType instead of Type)
+	sort.Slice(deals, func(i, j int) bool {
+		a, b := deals[i], deals[j]
+		if a.GoldType == b.GoldType {
+			for i := 0; i < len(a.Hand); i++ {
+				if a.Hand[i] != b.Hand[i] {
+					return CARD_SENIORITY_GOLD[a.Hand[i]] < CARD_SENIORITY_GOLD[b.Hand[i]]
+				}
+			}
+		}
+		return a.GoldType < b.GoldType
+	})
+	for i, deal := range deals {
+		gold += (i + 1) * deal.Bid
+	}
 	fmt.Printf("Gold: %d\n", gold)
 }
