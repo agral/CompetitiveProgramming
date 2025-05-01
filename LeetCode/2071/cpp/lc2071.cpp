bfce77d9ab95455f7ec6443ad494fbd5c381e900
agral <adam@gralin.ski>
2025-05-01 20:52:27 +0200

    Solve 2071/Maximum Number of Tasks You Can Assign
    Solved in C++.

--- LeetCode/2071/cpp/lc2071.cpp
+++ LeetCode/2071/cpp/lc2071.cpp
@@ -1,16 +1,76 @@
+#include <algorithm>
 #include <iostream>
+#include <map>
 #include <vector>
 
-// Runtime complexity:
-// Auxiliary space complexity:
 class Solution {
 public:
     int maxTaskAssign(std::vector<int>& tasks, std::vector<int>& workers,
                       int pills, int strength) {
         int ans = 0;
-        // TODO 
-        return ans;
+        int l = 0;
+        int r = std::min(tasks.size(), workers.size());
+        std::ranges::sort(tasks);
+        std::ranges::sort(workers);
 
+        // Returns true if numTasks can be completed using at most numPills
+        auto canComplete = [&](int numTasks, int numPills) {
+            std::map<int, int> sorted_workers;
+            // take `numTasks` strongest workers:
+            for (int i = workers.size() - numTasks; i < workers.size(); i++) {
+                ++sorted_workers[workers[i]];
+            }
+
+            // process `numTasks` easiest tasks, starting from the hardest one:
+            for (int i = numTasks-1; i >= 0; i--) {
+                // Find the least strong worker with own strength >= tasks[i].
+                // If such a worker is found, then no magic pill needs to be used,
+                // which is the preferred option.
+                auto it = sorted_workers.lower_bound(tasks[i]);
+                if (it != sorted_workers.end()) {
+                    // Use this worker.
+                    it->second -= 1;
+                    if (it->second == 0) {
+                        sorted_workers.erase(it);
+                    }
+                } else if (numPills > 0) {
+                    // Find the least strong worker with own strength >= tasks[i] - `strength`.
+                    // This is so that this worker can still complete the task,
+                    // when helped with a magic pill:
+                    it = sorted_workers.lower_bound(tasks[i] - strength);
+                    if (it != sorted_workers.end()) {
+                        // Use this worker and one magic pill.
+                        it->second -= 1;
+                        --numPills;
+                        if (it->second == 0) {
+                            sorted_workers.erase(it);
+                        }
+                    } else {
+                        // The current task can't be done as no worker is strong enough,
+                        // even when helped by the magic pill.
+                        return false;
+                    }
+                } else {
+                    // There are no magic pills left, and the current task can't be done
+                    // as no worker is strong enough on his own.
+                    return false;
+                }
+            }
+            // All the `numTasks` tasks have been completed successfully.
+            return true;
+        };
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (canComplete(mid, pills)) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return ans;
     }
 };
 
