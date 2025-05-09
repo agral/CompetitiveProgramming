eb2d52c2e9e49e0f5a35241d271704c7a792ac25
agral <adam@gralin.ski>
2025-05-09 21:16:12 +0200

    3343: Provide and test function for counting digits

--- LeetCode/3343/golang/lc3343.go
+++ LeetCode/3343/golang/lc3343.go
@@ -1,13 +1,16 @@
 package lc3343
 
+const MOD = 1_000_000_007
+const MOD64 = int64(MOD)
+
 // Runtime complexity:
 // Auxiliary space:
 func countBalancedPermutations(num string) int {
-	// Count the sum of all the digits:
+	// Count the sum of all the digits, and the frequency of individual digits:
 	sum := 0
-	for _, d := range num {
-		digit := int(d - '0')
-		sum += digit
+	digits := getDigits(num)
+	for digit, count := range digits {
+		sum += digit * count
 	}
 	if sum%2 == 1 {
 		// if the sum of all digits is odd, the numbers can not be divided into
@@ -27,4 +30,20 @@ func factorial(num int) int64 {
 		ans *= k
 	}
 	return ans
+}
+
+func getDigits(num string) []int {
+	ans := make([]int, 10)
+	for _, d := range num {
+		ans[int(d-'0')]++
+	}
+	return ans
+}
+
+func calcFreq(digits []int) {
+}
+
+func calcNumPermutations(digits []int) int64 {
+	ans := int64(1)
+	return ans // not implemented so far
 }
--- LeetCode/3343/golang/lc3343_test.go
+++ LeetCode/3343/golang/lc3343_test.go
@@ -1,6 +1,9 @@
 package lc3343
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func Test_countBalancedPermutations(t *testing.T) {
 	testcases := []struct {
@@ -37,6 +40,24 @@ func Test_factorial(t *testing.T) {
 		actual := factorial(tc.input)
 		if actual != tc.expected {
 			t.Errorf("factorial(%d) failed: want %d, got %d", tc.input, tc.expected, actual)
+		}
+	}
+}
+
+func Test_getDigits(t *testing.T) {
+	testcases := []struct {
+		input    string
+		expected []int
+	}{
+		{"1", []int{0, 1, 0, 0, 0, 0, 0, 0, 0, 0}},
+		{"123", []int{0, 1, 1, 1, 0, 0, 0, 0, 0, 0}},
+		{"123122111", []int{0, 5, 3, 1, 0, 0, 0, 0, 0, 0}},
+	}
+
+	for _, tc := range testcases {
+		actual := getDigits(tc.input)
+		if !slices.Equal(actual, tc.expected) {
+			t.Errorf("getDigits(%s) failed: want %v, got %v", tc.input, tc.expected, actual)
 		}
 	}
 }
