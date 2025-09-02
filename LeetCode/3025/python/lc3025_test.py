d8a9a337993a1a5ac5231c4128f5bafee6883466
agral <adam@gralin.ski>
2025-09-02 21:15:11 +0200

    Solve 3025/Find the Number of Ways to Place People I
    Solved in Python.

---
+++ LeetCode/3025/python/lc3025_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from lc3025 import Solution
+
+class Test_numberOfPairs(unittest.TestCase):
+    def test(self):
+        s = Solution()
+        self.assertEqual(s.numberOfPairs([[1, 1], [2, 2], [3, 3]]), 0)
+        self.assertEqual(s.numberOfPairs([[6, 2], [4, 4], [2, 6]]), 2)
+        self.assertEqual(s.numberOfPairs([[3, 1], [1, 3], [1, 1]]), 2)
+
+if __name__ == "__main__":
+    unittest.main()
