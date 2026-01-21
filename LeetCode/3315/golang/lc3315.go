3630fd7642d04fd5a1facc7a6d3b775357fd8f90
agral <adam@gralin.ski>
2026-01-21 22:09:10 +0100

    Solve 3315/Construct the Minimum Bitwise Array II
    Does copying a solution from yesterday's LC3314 count as solving?
    Anyway. Solved in Golang. An easy+.

---
+++ LeetCode/3315/golang/lc3315.go
@@ -0,0 +1,31 @@
+package lc3315
+
+// Runtime complexity: O(N * sum(log(nums[i]));
+// - hard to estimate, but higher than N, and surely lower than N^2.
+// - probably close to O(N*logN) or maybe to O(N).
+// Auxiliary space: O(N) (for holding the answer; otherwise O(1)).
+// Subjective level: easy+. Requires bit shifting, but nothing fancy.
+// Solved on: 2026-01-21,
+// by reusing the solution for LC3314 (it's the same! Even the testcases are a carbon copy!)
+func minBitwiseArray(nums []int) []int {
+	N := len(nums)
+	ans := make([]int, N)
+
+	getFirstSetBitOfLastGroupOfSetBits := func(num int) int {
+		first := 1
+		for num&first > 0 {
+			first <<= 1
+		} // note: this overshoots, i.e. actually points to the zero just before the actual answer.
+		return first >> 1
+	}
+
+	for i, num := range nums {
+		if num == 2 {
+			ans[i] = -1
+		} else {
+			ans[i] = num - getFirstSetBitOfLastGroupOfSetBits(num)
+		}
+	}
+
+	return ans
+}
---
+++ LeetCode/3315/golang/lc3315_test.go
@@ -0,0 +1,23 @@
+package lc3315
+
+import (
+	"slices"
+	"testing"
+)
+
+func Test_minBitwiseArray(t *testing.T) {
+	testcases := []struct {
+		nums     []int
+		expected []int
+	}{
+		{[]int{2, 3, 5, 7}, []int{-1, 1, 4, 3}},
+		{[]int{11, 13, 31}, []int{9, 12, 15}},
+	}
+
+	for i, tc := range testcases {
+		actual := minBitwiseArray(tc.nums)
+		if !slices.Equal(actual, tc.expected) {
+			t.Errorf("Testcase %02d (%v) failed: want %v, got %v", i+1, tc.nums, tc.expected, actual)
+		}
+	}
+}
