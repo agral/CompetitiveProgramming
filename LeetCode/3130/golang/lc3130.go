1b23f8e0b1a9ac187bea8e83eb55d436bf520ebd
agral <adam@gralin.ski>
2026-03-10 10:17:54 +0100

    Solve 3130/Find All Possible Stable Binary Arrays II
    Solved in Golang.

---
+++ LeetCode/3130/golang/lc3130.go
@@ -0,0 +1,49 @@
+package lc3130
+
+// Note: this solution is same as LC3129.
+// Runtime complexity: O(zero*one)
+// Auxiliary space: O(zero*one)
+// Subjective level: hard
+// Solved on: 2026-03-09
+func numberOfStableArrays(zero int, one int, limit int) int {
+	const MOD int64 = 1_000_000_007
+	// dp[z][o] holds the total count of stable arrays where:
+	// - the last number in the array is zero (dpZero) or one (dpOne).
+	// - the count of zeroes is z
+	// - the count of ones is o
+	dpZero := make([][]int64, zero+1)
+	dpOne := make([][]int64, zero+1)
+	for z := range zero + 1 {
+		dpZero[z] = make([]int64, one+1)
+		dpOne[z] = make([]int64, one+1)
+	}
+
+	for z := 0; z <= min(zero, limit); z++ {
+		dpZero[z][0] = 1
+	}
+
+	for o := 0; o <= min(one, limit); o++ {
+		dpOne[0][o] = 1
+	}
+
+	for z := 1; z <= zero; z++ {
+		for o := 1; o <= one; o++ {
+			dpZero[z][o] = dpZero[z-1][o] + dpOne[z-1][o]
+			if z-limit >= 1 {
+				dpZero[z][o] += MOD
+				dpZero[z][o] -= dpOne[z-limit-1][o]
+			}
+			dpZero[z][o] %= MOD
+
+			dpOne[z][o] = dpOne[z][o-1] + dpZero[z][o-1]
+			if o-limit >= 1 {
+				dpOne[z][o] += MOD
+				dpOne[z][o] -= dpZero[z][o-limit-1]
+			}
+			dpOne[z][o] %= MOD
+		}
+	}
+
+	ans := (dpZero[zero][one] + dpOne[zero][one]) % MOD
+	return int(ans)
+}
---
+++ LeetCode/3130/golang/lc3130_test.go
@@ -0,0 +1,24 @@
+package lc3130
+
+import "testing"
+
+func Test_numberOfStableArrays(t *testing.T) {
+	testcases := []struct {
+		zero     int
+		one      int
+		limit    int
+		expected int
+	}{
+		{1, 1, 2, 2},  // [1, 0], and [0, 1] are the only such stable binary arrays.
+		{1, 2, 1, 1},  // [1, 0, 1] - the only one.
+		{3, 3, 2, 14}, // [1, 1, 1, 0, 0, 0] and [0, 0, 0, 1, 1, 1] are not stable; the other 14 are.
+	}
+
+	for i, tc := range testcases {
+		actual := numberOfStableArrays(tc.zero, tc.one, tc.limit)
+		if actual != tc.expected {
+			t.Errorf("Testcase numberOfStableArrays#%02d (zero=%d, one=%d, limit=%d) failed: want %d, got %d",
+				i+1, tc.zero, tc.one, tc.limit, tc.expected, actual)
+		}
+	}
+}
