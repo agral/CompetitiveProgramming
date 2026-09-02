3b10b6949f6e1ab7dc8f9a7645b965618187c592
agral <adam@gralin.ski>
2026-09-02 07:54:50 +0200

    Solve 3875/Construct Uniform Parity Array I
    Solved in Golang. Or rather, solved the underlying theory on paper ---
    the actual solution is trivial in any language.

---
+++ LeetCode/3875/golang/lc3875.go
@@ -0,0 +1,9 @@
+package lc3875
+
+// Runtime complexity: O(1)
+// Auxiliary space: O(1)
+// Subjective level: trivially easy
+// Solved on: 2026-09-02
+func uniformArray(nums1 []int) bool {
+	return true
+}
